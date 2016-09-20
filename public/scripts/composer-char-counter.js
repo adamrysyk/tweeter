0082a22338d0ce554b284c0a03ecfbae33fa3854
adamrysyk <rysyk.adam@gmail.com>
2016-09-20 20:58:09 +0000

    finished character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  let $textarea = $(".new-tweet").find("textarea")
+  let $counter = $(".new-tweet").find(".counter")
+  console.log($textarea)
+  console.log($counter)
+
+  $('textarea').on("input", function () {
+    let tweetLength = $(this).val().length
+    $counter.text(140-tweetLength);
+    if (tweetLength > 140) {
+      $counter.addClass('red')
+    } else {
+      $counter.removeClass('red')
+    }
+  });
+
+})
